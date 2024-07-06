64c2651a62f3c363d6fa081bc0a0b1f683b8ef45
Koro <koro@kzxiv.net>
2024-07-06 16:18:20 -0400

    Async loop.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ mod logging;
 
 use arguments::Arguments;
 use clap::Parser;
-use configuration::Configuration;
-use log::debug;
+use configuration::{Configuration, Host, Ntfy};
+use log::{debug, warn};
 use logging::Logger;
-use std::error::Error;
+use std::{error::Error, sync::Arc};
+use tokio::{runtime::Builder, task::JoinSet};
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let args = Arguments::parse();
@@ -15,8 +16,29 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 	let cfg = Configuration::read(args.configuration.as_deref())?;
 
-	// TODO:
-	debug!("{:?}", cfg);
+	let rt = Builder::new_current_thread().build()?;
+	rt.block_on(execute(cfg));
 
 	Ok(())
+}
+
+async fn execute(cfg: Configuration) {
+	let ntfy = Arc::new(cfg.ntfy);
+
+	let mut set = JoinSet::new();
+	for host in cfg.hosts {
+		set.spawn(execute_one(ntfy.clone(), host));
+	}
+
+	if set.is_empty() {
+		warn!("no hosts defined")
+	}
+
+	while let Some(_) = set.join_next().await {}
+}
+
+async fn execute_one(ntfy: Arc<Ntfy>, host: Host) {
+	debug!("handling host {:?}", host.name);
+
+	// TODO:
 }
