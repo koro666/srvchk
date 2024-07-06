db7cc13418196eb45ece1bc446bf7783555aaf94
Koro <koro@kzxiv.net>
2024-07-06 09:39:40 -0400

    Add argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+struct Arguments {
+	#[command(flatten)]
+	verbose: clap_verbosity_flag::Verbosity,
+
+	#[arg(short, long, help = "Path to configuration file")]
+	configuration: Option<PathBuf>
+}
+
 fn main() {
+	let args = Arguments::parse();
+	println!("{:#?}", args);
 	// TODO:
 }
